53f5fd1bb5e37c4261fbd1596f6fe06cf854fd8c
David Spies <dnspies@gmail.com>
2020-11-22 14:52:44 -0800

    Use a single reference

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(once_cell)]
 
 use std::cell::Cell;
-use std::lazy::SyncOnceCell;
+use std::lazy::{OnceCell, SyncOnceCell};
 use std::sync::mpsc;
 use std::sync::{Arc, Weak};
 
@@ -10,20 +10,23 @@ pub struct LazyField<'a, S, T>(Arc<LazyFieldInner<'a, S, T>>);
 struct LazyFieldInner<'a, S, T> {
     value: SyncOnceCell<T>,
     constructor: Cell<Box<dyn FnOnce(&S) -> T + 'a>>,
-    holder: Cell<Weak<S>>,
+    holder: Weak<OnceCell<S>>,
 }
 
 impl<S, T> LazyFieldInner<'_, S, T> {
     fn get(&self) -> &T {
         self.value.get_or_init(|| {
-            let f = self.constructor.replace(Box::new(|_| {
-                panic!("Already constructed! (Constructor panicked?)")
-            }));
-            f(&*self
+            let f = self
+                .constructor
+                .replace(Box::new(|_| panic!("Field already constructed")));
+            let obj = &*self
                 .holder
-                .replace(Weak::new())
                 .upgrade()
-                .unwrap_or_else(|| panic!("Object not constructed")))
+                .unwrap_or_else(|| panic!("Object deleted"));
+            match obj.get() {
+                Some(ref x) => f(x),
+                None => panic!("Object not constructed"),
+            }
         })
     }
 }
@@ -34,37 +37,36 @@ impl<S, T> LazyField<'_, S, T> {
     }
     pub fn into_inner(self) -> T {
         Arc::try_unwrap(self.0)
-            .unwrap_or_else(|_| panic!("Other references being held?"))
+            .unwrap_or_else(|_| panic!("Other references to field being held?"))
             .value
             .into_inner()
-            .unwrap_or_else(|| panic!("Uninitialized!"))
+            .unwrap_or_else(|| panic!("Field uninitialized"))
     }
 }
 
 trait IsField<S> {
     fn resolve(&self);
-    fn set_holder(&self, holder: Weak<S>);
 }
 
 impl<'a, S, T> IsField<S> for LazyFieldInner<'a, S, T> {
     fn resolve(&self) {
         self.get();
     }
-    fn set_holder(&self, holder: Weak<S>) {
-        self.holder.replace(holder);
-    }
 }
 
-pub struct Register<'a, S>(mpsc::Sender<Weak<dyn IsField<S> + 'a>>);
+pub struct Register<'a, S> {
+    holder: Weak<OnceCell<S>>,
+    fields: mpsc::Sender<Weak<dyn IsField<S> + 'a>>,
+}
 
 impl<'a, S: 'a> Register<'a, S> {
     pub fn field<T: 'a, F: FnOnce(&S) -> T + 'a>(&self, f: F) -> LazyField<'a, S, T> {
         let result = Arc::new(LazyFieldInner::<'a, S, T> {
             value: SyncOnceCell::new(),
             constructor: Cell::new(Box::new(f)),
-            holder: Cell::new(Weak::new()),
+            holder: self.holder.clone(),
         });
-        self.0
+        self.fields
             .send(Arc::downgrade(&result) as Weak<dyn IsField<S>>)
             .unwrap();
         LazyField(result)
@@ -72,19 +74,23 @@ impl<'a, S: 'a> Register<'a, S> {
 }
 
 pub fn with_lazy_fields<'a, S, F: FnOnce(&mut Register<'a, S>) -> S>(f: F) -> S {
+    let holder = Arc::new(OnceCell::new());
     let (sender, receiver) = mpsc::channel();
-    let mut reg = Register(sender);
-    let res = Arc::new(f(&mut reg));
-    let received = receiver.into_iter().collect::<Vec<_>>();
-    for field in received.iter() {
-        if let Some(x) = field.upgrade() {
-            x.set_holder(Arc::downgrade(&res));
-        }
-    }
-    for field in received {
+    let mut reg = Register {
+        holder: Arc::downgrade(&holder),
+        fields: sender,
+    };
+    let res = f(&mut reg);
+    holder
+        .set(res)
+        .unwrap_or_else(|_| panic!("Already initialized?"));
+    for field in receiver {
         if let Some(x) = field.upgrade() {
             x.resolve()
         }
     }
-    Arc::try_unwrap(res).unwrap_or_else(|_| panic!("Unreachable! Other arcs still held"))
+    Arc::try_unwrap(holder)
+        .unwrap_or_else(|_| panic!("Other references to holder being held?"))
+        .take()
+        .unwrap_or_else(|| panic!("Uninitialized after calling set?"))
 }
